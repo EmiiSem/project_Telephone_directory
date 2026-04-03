b029fe8f88bfd1635a4f60a85dec58b376a38252
EmiiSem <just_dragon29@mail.ru>
2026-04-03 13:44:19 +0300

    Глобальные настройки, которые используются

---
+++ project_Telephone_directory/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using System.Collections.ObjectModel;
+global using System.Globalization;
+global using System.Text.Json;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.ApplicationModel;
+global using project_Telephone_directory;
