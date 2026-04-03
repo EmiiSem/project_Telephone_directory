034265c506eab506409b9fe01874c036ae385c45
EmiiSem <just_dragon29@mail.ru>
2026-04-03 13:37:10 +0300

    Добавлены роуты

---
+++ project_Telephone_directory/ShellRoutes.cs
@@ -0,0 +1,12 @@
+namespace project_Telephone_directory;
+
+public static class ShellRoutes
+{
+    public const string Home = "Home";
+
+    public const string Contacts = "Contacts";
+
+    public const string Search = "Search";
+
+    public const string Settings = "Settings";
+}
