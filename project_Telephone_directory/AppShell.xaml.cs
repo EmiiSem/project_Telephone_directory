582701d093c94033819eafbabbd6977245eba672
EmiiSem <just_dragon29@mail.ru>
2026-04-03 12:05:12 +0300

    Добавьте файлы проекта.

---
+++ project_Telephone_directory/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace project_Telephone_directory
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ project_Telephone_directory/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace project_Telephone_directory
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
